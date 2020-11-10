235219f1cf02906ce4ef70437848050441170e10
retrohv4 <70591293+retrohv4@users.noreply.github.com>
2020-11-10 09:26:17 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log("Bot iniciado com sucesso.")
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (message.channel.type == 'dm') return;
+  if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
+  if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+
+  const args = message.content
+    .trim().slice(config.prefix.length)
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  try {
+    const commandFile = require(`./commands/${command}.js`)
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error('Error =>' + err);
+  }
+});
+
+
+client.login(process.env.TOKEN);
